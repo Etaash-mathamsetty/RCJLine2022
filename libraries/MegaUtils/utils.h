7282531762b381c6bb0609c0c457a91c42312a31
Etaash-mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2022-03-06 16:55:18 -0500

    use pointers instead of references
    fixes an error

--- libraries/MegaUtils/utils.h
+++ libraries/MegaUtils/utils.h
@@ -6,22 +6,22 @@
 
 namespace utils{
 
-Motor& motor;
-Motor& motor2;
+Motor* motor;
+Motor* motor2;
 
-void setMotors(Motor& _motor1, Motor& _motor2){
+void setMotors(Motor* _motor1, Motor* _motor2){
 	motor = _motor1;
 	motor2 = _motor2;
 }
 
 void forward(int rightSpeed, int leftSpeed) {
-  motor.run(-rightSpeed);
-  motor2.run(leftSpeed);
+  motor->run(-rightSpeed);
+  motor2->run(leftSpeed);
 }
 
 void stopMotors(){
-	motor.stop();
-	motor2.stop();
+	motor->stop();
+	motor2->stop();
 }
 
 };
