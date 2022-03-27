05984c3b9250bd1cfee05e995197d14d6d205818
RejectedRyan <98713672+RejectedRyan@users.noreply.github.com>
2022-03-27 17:47:51 -0400

    Fixed the triangle detection function!

--- Evacuation.ino
+++ Evacuation.ino
@@ -1,3 +1,28 @@
+void setup()
+{
+  attachInterrupt(digitalPinToInterrupt(2), Encoder3, CHANGE);
+  attachInterrupt (digitalPinToInterrupt(18), Encoder1, CHANGE);
+  attachInterrupt (digitalPinToInterrupt(3), Encoder2, CHANGE);
+  int i; 
+  Serial.begin(9600);
+  Wire.begin();  
+  
+  for(i = 0; i <= 2; i++)  {
+  tcaselect(i); 
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1) {}
+  }
+
+
+  sensor.startContinuous(); 
+  }
+ 
+}
+
+
 void Evac()  {
 
 int case = 0, tri;
@@ -151,28 +176,33 @@ int case = 0, tri;
 }
 int triangleDETECT() {
   //x = the differnce betweeen the location of the top and the bottom
-  int tof1, tof2, keydifference;
+  int toffront, tofback, keydifference;
 
-  tcaselect(0);
-  tof1 = sensor.readRangeContinuousMillimeters();
   tcaselect(1);
-  tof2 = sensor.readRangeContinuousMillimeters();
-  keydifference = tof2 - tof1;
+  toffront = sensor.readRangeContinuousMillimeters();
+  tcaselect(2);
+  tofback = sensor.readRangeContinuousMillimeters();
 
+  keydifference = tofback - toffront;   
+ 
+  Serial.println(keydifference); 
+  if(keydifference >= 40){ 
 
+    Serial.println("triangle1");  
+    return(1); 
+    
+  } 
+  else if(keydifference <= -40){ 
 
-  if (keydifference > 50) {
-
-    return 1; // triangle
-
-  }
-  else if (keydifference < -50) {
-
-    return 1; // triangle
-
-  }
-  else
-    return 2;  // means that there is a flat wall
+    Serial.println("triangle2");  
+    return(2); 
+    
+  } 
+  else 
+    Serial.println("flat wall"); 
+  delay(100);  
+  return(3); 
+ 
 
 }
 int CheckWall() {
